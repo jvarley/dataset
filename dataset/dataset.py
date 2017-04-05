13e58dc6f9e62e0ecf3f43dc04195eb131850bcd
Jake Varley <jakevarley@gmail.com>
2017-04-05 11:09:41 -0400

    initial pass

---
+++ dataset/dataset.py
@@ -0,0 +1,51 @@
+import random
+import os
+import yaml
+
+class Dataset(object):
+
+	def __init__(self, dataset_name, dataset_dir="/srv/datasets/"):
+		self.dataset_dir = dataset_dir
+		self.dataset_name = dataset_name
+
+		self.dataset_fullpath = self.dataset_dir + self.dataset_name
+
+		#split: [item_filepaths]
+		self.items_cache = {}
+
+	def get_info(self):
+		yaml_fullpath = self.dataset_fullpath + "info.yaml"
+		data = yaml.load(open(yaml_file, "r"))
+		return data
+
+
+	def get_item(self, split, idx=None):
+		#if split not in cache, add it
+		if not self.items_cache.has_key(split):
+			split_fullpath = self.dataset_fullpath + "/" + split + ".txt"
+			f = open(split_fullpath, 'r')
+			lines = f.readlines()
+			items = [None]*len(lines)
+			for i, line in enumerate(lines):
+				items[i] = line.replace("\n", "").split(", ")
+
+			self.items_cache[split] = items
+
+		items = self.items_cache[split]
+		
+		#if idx not specified, get a random one
+		if not idx:
+			idx = random.randrange(0, len(items))
+
+		return items[idx]
+
+	def get_num_items(self, split):
+		split_fullpath = self.dataset_fullpath + "/" + split + ".txt"
+		f = open(split_fullpath, 'r')
+		return len(f.readlines())
+
+	def get_split_names(self):
+		txt_files = [x for x in os.listdir(self.dataset_fullpath) if ".txt" in x]
+
+		splits = [x.replace(".txt", "") for x in txt_files]
+		return splits
---
+++ dataset/dataset_writer.py
@@ -0,0 +1,48 @@
+import os
+import yaml
+
+
+class DatasetWriter(object):
+
+    """
+    This class creates a Dataset Directory in /srv/datasets to be used for training
+    This will use the flat files sitting in /srv/data
+    """
+    def __init__(self, dataset_name, dataset_dir="/srv/datasets/"):
+        self.dataset_dir = dataset_dir
+        self.dataset_name = dataset_name
+
+        self.dataset_fullpath = self.dataset_dir + self.dataset_name
+
+        if not os.path.exists(self.dataset_fullpath):
+            os.mkdir(self.dataset_fullpath)
+
+    """
+    This should be the filenames for a training example
+    ex:
+    [/srv/data/shape_completion_data/grasp_database/lime_poisson_000/pointclouds/_2_3_9_x.pcd,
+    /srv/data/shape_completion_data/grasp_database/lime_poisson_000/pointclouds/_2_3_9_y.pcd]
+    """
+    def add_item(self, split, filename_list):
+        split_fullpath = self.dataset_fullpath + "/" + split + ".txt"
+        f = open(split_fullpath, 'a')
+        line = ", ".join(filename_list) + "\n"
+        f.write(line)
+        f.close()
+
+    """
+    Use this to add key value information to the info.yaml for the dataset
+    ex patch_size: 40
+    """
+    def add_info_item(self, key, value):
+        yaml_fullpath = self.dataset_fullpath + "info.yaml"
+        if os.path.exists(yaml_fullpath):
+            data = yaml.load(open(yaml_file, "r"))
+        else:
+            data = {}
+
+        #add the new information to the yaml dict
+        data[key] = value
+
+        with open(yaml_fullpath, "w") as outfile:
+            yaml.dump(data, outfile, default_flow_style=True)
